3892755bb1d3f9e466cf0ccd901c6c9548ad8364
Kobe Jackson <123972668+kobejackson98@users.noreply.github.com>
2023-02-10 15:51:33 -0600

    Updating CSS

---
+++ script.js
@@ -0,0 +1,22 @@
+let a = 1;
+
+function lesson() {
+    let b =2;
+    console.log(a);
+    console.log(b);
+
+}
+function whodat() {
+
+    let name = prompt ("What is your name?");
+    //let name  = console.Readline();
+    if (name !=Kobe) {
+        alert ("ERROR");
+    } else {
+        alert(" Hello," + name);
+    }
+
+    confirm()
+ }
+
+console.log(b);
